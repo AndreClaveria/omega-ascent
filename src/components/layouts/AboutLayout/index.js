0d4b8a1df556608de79f2d52390d3de739f3a081
DarylNyd <95940306+DarylNyd@users.noreply.github.com>
2023-05-11 11:00:00 +0200

    start about page

--- src/components/layouts/AboutLayout/index.js
+++ src/components/layouts/AboutLayout/index.js
@@ -6,6 +6,9 @@ import Header from '@/components/partials/Header';
 const Index = ({ children }) => {
   return (
     <div className={style.global_cont}>
+      <div className={style.stars}></div>
+      <div className={style.stars2}></div>
+      <div className={style.stars3}></div>
       <Header />
       {children}
     </div>
