db4b11ad14aff449f953934afc4109be72c134ff
AndreClaveria <andreclaveria2002@gmail.com>
2023-05-21 12:52:20 +0200

    test unitaire hello world

--- src/components/UI/About_Scroll/index.js
+++ src/components/UI/About_Scroll/index.js
@@ -15,8 +15,15 @@ const Index = () => {
         imagesrc={Avatar1}
         title="Florian Moracchini"
         description="
-        Hi there, I'm The_Leon, the leader of the Omega Ascent project. I'm excited to introduce you to our world of animation and 3D.  Our main goal with this project was to have fun and create something that we truly enjoy, and I'm proud to say that it has been a successful journey so far. We've poured countless hours of hard work and dedication into this project, and I hope you'll enjoy it as much as we do.
-        As for me, I'm a passionate web developer and designer, with a strong interest in 3D and animation. I'm always eager to learn and improve my skills in these areas, and the Omega Ascent project has provided me with the perfect opportunity to do so. I believe that the combination of web development, design, and 3D/animation is a powerful one, and I'm excited to see where this journey takes us. 
+        Hi there, I'm The_Leon, the leader of the Omega Ascent project. I'm excited to introduce you 
+        to our world of animation and 3D.  Our main goal with this project was to have 
+        fun and create something that we truly enjoy, and I'm proud to say that it has been a successful 
+        journey so far. We've poured countless hours of hard work and dedication into this project, and 
+        I hope you'll enjoy it as much as we do.
+        As for me, I'm a passionate web developer and designer, with a strong interest in 3D and animation. 
+        I'm always eager to learn and improve my skills in these areas, and the Omega Ascent project has provided me
+        with the perfect opportunity to do so. I believe that the combination of web development, design, 
+        and 3D/animation is a powerful one, and I'm excited to see where this journey takes us. 
         Thanks for stopping by, and I hope you'll enjoy exploring our world of animation and 3D with us.
         "
         comment="Like a lion, fearless and proud, we venture into the realm of creativity, conquering new horizons with every stride"
---
+++ src/server.js
@@ -0,0 +1,20 @@
+function startServer() {
+  console.log('Server is running');
+}
+
+function stopServer() {
+  console.log('Server is not running');
+}
+
+const condition = 'hello world';
+
+if (condition === 'hello world') {
+  startServer();
+} else {
+  stopServer();
+}
+
+module.exports = {
+  startServer,
+  stopServer,
+};
